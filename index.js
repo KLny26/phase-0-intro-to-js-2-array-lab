793ce189c35df5b12e775c7908fe8fdc6a05f83e
kristenli <kyli2005@gmail.com>
2022-04-18 22:09:08 -0400

    commit tree 567f4ed1a78342e027851648b532a3d6b1fe17c7

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// function beforeEach(){
+   //cats.length = 0
+//}
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+   let appendCat = ([...cats, "Broom"]);
+   return appendCat
+}
+
+function prependCat(name){
+    let prependCat = (["Arnold", ...cats]);
+    return prependCat
+}
+
+function removeLastCat(){
+    let removeLastCat = cats.slice(0, cats.length-1);
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    let removeFirstCat = cats.slice(-2);
+    return removeFirstCat
+}
